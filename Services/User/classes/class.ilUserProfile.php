f25275666da0c0bc2251602214c78e62bbc5d380
Christian Bogen <christian.bogen@tik.uni-stuttgart.de>
2023-11-08 14:32:11 +0100

    Patch für AutoGenerateUsername

--- Services/User/classes/class.ilUserProfile.php
+++ Services/User/classes/class.ilUserProfile.php
@@ -555,7 +555,12 @@ class ilUserProfile
                             $val->setValue($a_user->getLogin());
                         }
                     }
-                    $a_form->addItem($val);
+                    // BEGIN Patch hide username in registration forms
+                    // $a_form->addItem($val);
+                    if(!self::$mode == self::MODE_REGISTRATION) {
+                        $a_form->addItem($val);
+                    }
+                    //END Patch
                     break;
 
                 case "text":
