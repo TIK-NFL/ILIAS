af5d4a9b50834122961be7c6bd0b125def7ee251
Christian Bogen <christian.bogen@tik.uni-stuttgart.de>
2024-11-07 16:11:38 +0100

    Revert "Anpassung Patch für AutoGenerateUsername"
    This reverts commit 22a2a243fe1ca2b41a02ba4a37421cdd9d727ba2.

--- Services/User/classes/Profile/class.ilUserProfile.php
+++ Services/User/classes/Profile/class.ilUserProfile.php
@@ -67,11 +67,6 @@ class ilUserProfile
                 in_array($f, $this->skip_fields)) {
                 continue;
             }
-            // BEGIN PATCH AutogenerateUsername
-            if ($f == 'username') {
-                continue;
-            }
-            // END PATCH AutogenerateUsernam
             $fields[$f] = $p;
         }
         return $fields;
@@ -460,13 +455,8 @@ class ilUserProfile
             $login_input->setSize(255);
             $login_input->setRequired(true);
         }
-        // BEGIN Patch hide username in registration forms
-        if (
-            $user !== null &&
-            !$this->mode == self::MODE_REGISTRATION
-        ) {
-        //if ($user !== null) {
-        // END Patch
+
+        if ($user !== null) {
             $login_input->setValue($user->getLogin());
         }
         return $login_input;
