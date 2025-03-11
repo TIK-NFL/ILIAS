56e52f0644186ad877021ca80f9b63d5ee8090e4
Christian Bogen <christian.bogen@tik.uni-stuttgart.de>
2025-03-11 14:17:11 +0100

    Revert "Exercise: Fix ZIP uploads for Windows clients"
    This reverts commit 9a06456a59ed013e9119ea1a2fda41097659576e.
    
    Sollte nicht mehr nötig sein, da offiziell an anderer Stelle behoben.

--- Modules/Exercise/classes/class.ilExerciseManagementGUI.php
+++ Modules/Exercise/classes/class.ilExerciseManagementGUI.php
@@ -1816,7 +1816,7 @@ class ilExerciseManagementGUI
                 "rc_id",
                 "",
                 1,
-                ["application/zip", "application/x-compressed", "application/x-zip-compressed"]
+                ["application/zip"]
             );
         return $form;
     }
