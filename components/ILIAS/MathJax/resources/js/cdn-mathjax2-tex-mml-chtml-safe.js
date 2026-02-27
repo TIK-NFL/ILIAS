93310fd73ab8fe3d30cd4a1c41acf0f581a1a655
Christian Bogen <christian.bogen@tik.uni-stuttgart.de>
2026-02-27 15:35:14 +0100

    MathJax2: Zoom-Click

--- components/ILIAS/MathJax/resources/js/cdn-mathjax2-tex-mml-chtml-safe.js
+++ components/ILIAS/MathJax/resources/js/cdn-mathjax2-tex-mml-chtml-safe.js
@@ -18,6 +18,10 @@
  * @see https://docs.mathjax.org/en/latest/web/configuration.html#configuring-and-loading-in-one-script
  */
 
+MathJax.Hub.Config({
+  menuSettings: {zoom: "Click"};
+});
+
 (function () {
   const script = document.createElement('script');
   script.src = 'https://cdn.jsdelivr.net/npm/mathjax@2.7.9/MathJax.js?config=TeX-AMS-MML_HTMLorMML,Safe';
