fc6b326e04351632015d5334ab6f8c93265114ac
Christian Bogen <christian.bogen@tik.uni-stuttgart.de>
2026-03-23 16:07:07 +0100

    Revert "Patch für AutoGenerateUsername"
    This reverts commit 17895842a47260fc6abaeb12be50c86ef13063ec.

--- components/ILIAS/Registration/classes/class.ilAccountRegistrationGUI.php
+++ components/ILIAS/Registration/classes/class.ilAccountRegistrationGUI.php
@@ -163,14 +163,6 @@ class ilAccountRegistrationGUI
         $this->lng->loadLanguageModule("user");
         // add fields to form
         $this->user_profile->addStandardFieldsToForm($this->form, null, $custom_fields);
-        // BEGIN Patch hide username in registration forms
-        $xagu_plugin_settings = new ilSetting('xagu');
-        $contexts = explode(';', $xagu_plugin_settings->get('xagu_contexts', ''));
-        $is_registraton_context_enabled = in_array(1, is_array($contexts) ? $contexts : []);
-        if ($is_registraton_context_enabled) {
-            $this->form->removeItemByPostVar("username");
-        }
-        // END Patch
         unset($custom_fields);
 
         // set language selection to current display language
@@ -323,43 +315,24 @@ class ilAccountRegistrationGUI
             $form_valid = false;
         }
 
-        // BEGIN Patch hide username in registration forms
-        $xagu_plugin_settings = new ilSetting('xagu');
-        $contexts = explode(';', $xagu_plugin_settings->get('xagu_contexts', ''));
-        $is_registraton_context_enabled = in_array(1, is_array($contexts) ? $contexts : []);
-
         // validate username
-
-        if ($is_registraton_context_enabled) {
-            $login = "registration_" . time();
-        }
-
-        if (!$is_registraton_context_enabled) {
-            $login_obj = $this->form->getItemByPostVar('username');
-            $login = $this->form->getInput("username");
-        }
-
+        $login_obj = $this->form->getItemByPostVar('username');
+        $login = $this->form->getInput("username");
         if (!ilUtil::isLogin($login)) {
-            if (!$is_registraton_context_enabled) {
-                $login_obj->setAlert($this->lng->txt("login_invalid"));
-            }
+            $login_obj->setAlert($this->lng->txt("login_invalid"));
             $form_valid = false;
         }
 
         // We should use the HTTP request stretching mechanisms here, according to Mantis #32037
-        $username_checked_and_register_callback = function () use (&$form_valid, $login, $login_obj, $valid_role, $is_registraton_context_enabled) {
+        $username_checked_and_register_callback = function () use (&$form_valid, $login, $login_obj, $valid_role) {
             if ($form_valid) {
                 if (ilObjUser::_loginExists($login)) {
-                    if (!$is_registraton_context_enabled) {
-                        $login_obj->setAlert($this->lng->txt("login_exists"));
-                    }
+                    $login_obj->setAlert($this->lng->txt('login_exists'));
                     $form_valid = false;
                 } elseif ((int) $this->settings->get('allow_change_loginname') &&
                     (int) $this->settings->get('reuse_of_loginnames') === 0 &&
                     ilObjUser::_doesLoginnameExistInHistory($login)) {
-                    if (!$is_registraton_context_enabled) {
-                        $login_obj->setAlert($this->lng->txt('login_exists'));
-                    }
+                    $login_obj->setAlert($this->lng->txt('login_exists'));
                     $form_valid = false;
                 }
             }
@@ -369,7 +342,6 @@ class ilAccountRegistrationGUI
                 $this->distributeMails($password);
             }
         };
-        // END Patch
 
         if (($register_duration = $this->settings->get('registration_duration')) !== null) {
             $duration = $this->http->durations()->callbackDuration((int) $register_duration);
@@ -422,15 +394,6 @@ class ilAccountRegistrationGUI
                 }
             }
         }
-
-        // BEGIN Patch hide username in registration forms
-        $xagu_plugin_settings = new ilSetting('xagu');
-        $contexts = explode(';', $xagu_plugin_settings->get('xagu_contexts', ''));
-        $is_registraton_context_enabled = in_array(1, is_array($contexts) ? $contexts : []);
-        if ($is_registraton_context_enabled) {
-            $this->userObj->setLogin('login_' . time());
-        }
-        // END Patch
 
         $this->userObj->setFullName();
 
