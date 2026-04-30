030301bbaa0945c80bb9071cb4666302e49862d7
Christian Bogen <christian.bogen@tik.uni-stuttgart.de>
2026-04-30 13:39:12 +0200

    Revert "IRSS-Patch von Martin, s. https://mantis.ilias.de/view.php?id=31664#c125148"
    This reverts commit 52464d6d2c1235e5c5f9395ce1dc1bf5430d0925.

--- components/ILIAS/ResourceStorage/src/StorageHandler/FileSystemBased/AbstractFileSystemStorageHandler.php
+++ components/ILIAS/ResourceStorage/src/StorageHandler/FileSystemBased/AbstractFileSystemStorageHandler.php
@@ -295,14 +295,7 @@ abstract class AbstractFileSystemStorageHandler implements StorageHandler
         $first_level = strtok($container_path, "/");
         if (!empty($first_level)) {
             $full_first_level = $storage_path . '/' . $first_level;
-            // Patch von Martin Gorgas
-            // $number_of_files = $this->fs->finder()->files()->in([$full_first_level])->count();
-            $has_files = false;
-            foreach ($this->fs->finder()->files()->in([$full_first_level]) as $file) {
-                $has_files = true;
-                break;
-            }
-            // Ende Patch
+            $number_of_files = $this->fs->finder()->files()->in([$full_first_level])->count();
             if ($number_of_files === 0) {
                 $this->fs->deleteDir($full_first_level);
             }
